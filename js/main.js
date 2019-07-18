0a3dec83aebf5f988131c4ba75d70bcda306670b
Shawn-Nevermore <shawnxu2016sg@gmail.com>
2019-07-18 21:17:29 +0800

    渲染md

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-var code = document.querySelector("#code")
-var styleTag = document.querySelector("#styleTag")
+!(function() {
+    let code = document.querySelector('#code')
+    let styleTag = document.querySelector('#styleTag')
 
-var css1 = `/**
+    let css1 = `/**
  * Hello，你好，欢迎来到这里。
  * 我叫许骁，是一名前端求职者，下面我来自我介绍。
  * 
@@ -66,7 +67,7 @@ html {
 body {
     perspective: 1000px;
 }
-  
+    
 #code {
     transform: translateX(98.5%) rotateY(-10deg);
     transform-origin: right;
@@ -77,9 +78,9 @@ body {
  * 那么接下来，就正式开始写我的简历吧
  * 首先，我需要一张白纸
  */
-`
+    `
 
-var css2 = `
+    let css2 = `
 #paper {
     background: #ddd;
     width: 48%;
@@ -91,60 +92,125 @@ var css2 = `
     transform-origin: left;
     max-height: 94.5%;
     white-space: pre-wrap;
+    overflow: auto;
 }
-`
+    `
 
-var md = `
+    let md = `
 # 许骁
---------
 求职岗位：前端开发
-毕业院校：电子科技大学
+毕业院校：电子科技大学-软件工程
 
+## 技能树
+- 熟悉HTML(5)/CSS(3)/JavaScript
+- 熟悉Ajax、DOM、HTTP协议
+- 熟练使用Vue
+- 用过Webpack打包工具
+- 有移动端开发经验
+- 熟练使用Linux、Git工作流
+- 计算机基础知识扎实
 
-# 项目经历
-# 
-`
+## 项目经历
+1. [导航狂魔](https://uiao.info/myBookmarks/)
+2. [小画板](https://uiao.info/canvas-demo/)
+3. [皮卡丘]()
+4. [纯CSS八卦](https://uiao.info/YinYang/)
+5. [许骁个站](https://uiao.info/)
 
-writeTo("", css1, () => {
-    createPaper(() => {
-        writeTo(css1, css2, () => {
-            writeMd(md)
+## 足迹
+1. [简书blog](https://www.jianshu.com/u/9e10fddb2bba)
+2. [github](https://github.com/Shawn-Nevermore)
+
+## 联系方式
+email：uiao.xx@gmail.com uiao.xx@qq.com
+wechat：john60155
+    `
+    let css3 = `
+/**
+ * markdown写完了，是时候把它转化为HTML了
+ */
+    `
+
+    // 初尝回调地狱
+    writeTo('', css1, () => {
+        createPaper(() => {
+            writeTo(css1, css2, () => {
+                writeMd(md, () => {
+                    writeTo(css1 + css2, css3, () => {
+                        renderMd(() => {})
+                    })
+                })
+            })
         })
     })
-})
 
-function createPaper(fn) {
-    var paper = document.createElement("div")
-    paper.id = "paper"
-    code.insertAdjacentElement("afterend", paper)
-    fn.call()
-}
+    /************************ Helper ************************************/
+    function createPaper(fn) {
+        let paper = document.createElement('div')
+        paper.id = 'paper'
+        let content = document.createElement('pre')
+        content.className = 'content'
+        paper.appendChild(content)
+        code.insertAdjacentElement('afterend', paper)
+        fn && fn.call()
+    }
 
-function writeTo(prefix, cssCode, fn) {
-    code.innerHTML = prefix || ""
-    let n = 0
-    let id = setInterval(() => {
-        n += 1
-        code.innerHTML = Prism.highlight(prefix + cssCode.substring(0, n), Prism.languages.css)
-        styleTag.textContent = prefix + cssCode.substring(0, n)
-        code.scrollTop = code.scrollHeight
-        if (n >= cssCode.length) {
-            clearInterval(id)
-            fn.call()
-        }
-    }, 20)
-}
+    /**
+     * 写 CSS 代码到 #code 和 #styleTag 中
+     *
+     * @param {*} prefix
+     * @param {*} cssCode
+     * @param {*} fn
+     */
+    function writeTo(prefix, cssCode, fn) {
+        code.innerHTML = prefix || ''
+        let n = 0
+        let id = setInterval(() => {
+            n += 1
+            code.innerHTML = Prism.highlight(prefix + cssCode.substring(0, n), Prism.languages.css)
+            styleTag.textContent = prefix + cssCode.substring(0, n)
+            code.scrollTop = code.scrollHeight
+            if (n >= cssCode.length) {
+                clearInterval(id)
+                fn && fn.call()
+            }
+        }, 0)
+    }
 
-function writeMd(md, fn) {
-    let paper = document.querySelector("#paper")
-    let n = 0
-    let id = setInterval(() => {
-        n += 1
-        paper.innerHTML = md.substring(0, n)
-        paper.scrollTop = paper.scrollHeight
-        if (n >= md.length) {
-            clearInterval(id)
-            fn.call()
-        }
-    }, 50)
-}
+    /**
+     * 写 markdown 到 paper 中
+     *
+     * @param {*} md
+     * @param {*} fn
+     */
+    function writeMd(md, fn) {
+        let mdContent = document.querySelector('#paper > .content')
+        let n = 0
+        let id = setInterval(() => {
+            n += 1
+            mdContent.innerHTML = md.substring(0, n)
+            mdContent.scrollTop = mdContent.scrollHeight
+            if (n >= md.length) {
+                clearInterval(id)
+                fn && fn.call()
+            }
+        }, 0)
+    }
+
+    /**
+     * 渲染 markdown 为 HTML
+     *
+     * @param {*} fn
+     */
+    function renderMd(fn) {
+        // let content = document.querySelector('#paper > .content')
+        // content.innerHTML = marked(md)
+
+        let div = document.createElement('div')
+        div.className = 'html markdown-body'
+        div.innerHTML = marked(md)
+        let markdownContainer = document.querySelector('#paper > .content')
+        markdownContainer.replaceWith(div)
+        fn && fn.call()
+    }
+})()
